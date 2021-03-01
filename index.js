511b00bccab4de04c9137e177477eea3c378609e
Charles Lai <charlesangeloplai@gmail.com>
2021-03-01 05:27:27 -0500

    commit tree 83db27379385d9489dadd2f85660f9186143befc

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  let userData = {
+    name: name,
+    email: email,
+  };
+
+  let userConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
+
+  return fetch("http://localhost:3000/users", userConfig)
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (user) {
+      document.body.innerHTML = user["id"];
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
